ff071030ecc5d35d0a78564d9bece29dddf00d50
Rafael VERISSIMO <raveriss@f2r11s3.clusters.42paris.fr>
2025-06-17 16:28:37 +0200

    Re-start

--- utils/cube_utils.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-import pycuber as pc
-
-def apply_scramble(scramble: str) -> pc.Cube:
-    cube = pc.Cube()
-    if scramble:
-        cube(pc.Formula(scramble))
-    return cube
